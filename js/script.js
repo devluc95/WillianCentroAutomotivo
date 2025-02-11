c72eddde3cae0f0117318e2a7c0229dcb77fa15b
devluc95 <lucas.alm.bsb@gmail.com>
2025-02-10 23:01:57 -0300

    inserindo imagens

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
-// Smooth scrolling
+// Scroll suave
 $(document).ready(function () {
     $('a.nav-link').on('click', function (event) {
         var target = $(this).attr('href');
         $('html, body').animate({
-            scrollTop: $(target).offset().top - 70 // Adjust for fixed header
+            scrollTop: $(target).offset().top - 70 // Ajuste para a navbar fixa
         }, 800);
         event.preventDefault();
     });
+});
+
+// Inicializar componentes do Bootstrap
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="popover"]').popover();
 });
